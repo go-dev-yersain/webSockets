d1a0b985669a43943efb230d56b00b751fbe100c
Yersain <ErsainIs@halykbank.kz>
2019-12-23 23:01:08 +0600

    init cm

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/confluent-kafka-go/kafka"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/melody"
+	"net/http"
+	"sync"
+)
+
+type JsonResponse struct {
+	Authorization string `json:"Authorization"`
+	ClientId string `json:"ClientId"`
+}
+
+var wsConnections map[string]*melody.Session
+
+
+func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+
+	kafkaMessageCh := make(chan Message)
+
+	go webSocketListener(wg)
+	go kafkaConsumer(wg)
+}
+
+func webSocketListener(wg *sync.WaitGroup){
+	defer wg.Done()
+	wsConnections = make(map[string]*melody.Session)
+	r := gin.Default()
+	m := melody.New()
+
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "index.html")
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m := new(JsonResponse)
+		json.Unmarshal(msg, m)
+		wsConnections[m.ClientId] = s
+		fmt.Println(m, wsConnections)
+	})
+
+
+	m.HandleConnect(func(s *melody.Session){
+
+	})
+
+
+
+	r.Run(":5000")
+
+}
+
+func kafkaConsumer(wg *sync.WaitGroup) {
+	defer wg.Done()
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	defer c.Close()
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"testTopic", "^aRegex.*[Tt]opic"}, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			m := Message{}
+			json.Unmarshal(msg.Value, &m)
+			fmt.Println(m)
+		} else {
+			// The client will automatically try to recover from all errors.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+}
+
+type Message struct {
+	Event string          `json:"event"`
+	Data  json.RawMessage `json:"data"`
+	Keys  struct {
+		ProcessId string `json:"process_id"`
+		PublicId  string `json:"public_id"`
+		ClientId  string `json:"client_id"`
+		Token     string `json:"token"`
+	} `json:"keys"`
+}
+
+//type keys struct{
+//	processId string `json:"processId"`
+//	publicId string `json:"publicId"`
+//	clientId string `json:"clientId"`
+//	token string 	`json:"token"`
+//}
+//
+//{"event":"push","keys":{"process_id":"p1","public_id":"pb1","client_id":"cl1","token":"lkfdnvlkdsjf"}}
+
+//{"id":"numero uno","type":"transaction","data":{"id":"numero uno","type":"transaction","amount":"1000","currency":"usd"}}"`
+
+//{"title":"The Matrix","year":1999,"cast":["Keanu Reeves","Laurence Fishburne","Carrie-Anne Moss","Hugo Weaving","Joe Pantoliano"],"genres":["Science Fiction"]}
